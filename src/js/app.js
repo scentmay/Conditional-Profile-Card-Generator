1820c43aa617af14003f0e9ad9d9e707c046deaf
scentmay <scentmay@gmail.com>
2022-06-17 19:40:04 +0000

    Terminado proyecto, condicionales funcionando así como cuando no hay datos

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,26 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : "Introduce tu nombre"} ${
+    variables.lastname ? variables.lastname : " y apellido"
+  }</h1>
+          <h2>${variables.role ? variables.role : "Introduce tu role"}</h2>
+          <h3>${variables.country ? variables.country : "País"} ${
+    variables.city ? variables.city : "Ciudad"
+  }</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://www.instagram.com/${
+              variables.instagram
+            }/"><i class="fa-brands fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
